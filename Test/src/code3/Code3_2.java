f3867111a8325e30d56bd88bbfff5a2a385a4f09
JJJJJoy <403239273@qq.com>
2021-10-13 13:37:51 +0800

    book Code3(1-4)

---
+++ Test/src/code3/Code3_2.java
@@ -0,0 +1,13 @@
+package code3;
+import java.util.Scanner;
+public class Code3_2 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter an integer: ");
+		int number = input.nextInt();
+		if (number%5==0)
+			System.out.println("HiFive!");
+		if (number%2==0)
+			System.out.println("HiEven!");
+	}
+}
---
+++ Test/src/code3/Code3_4.java
@@ -0,0 +1,25 @@
+package code3;
+import java.util.Scanner;
+public class Code3_4 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter weight in pounds: ");
+		double weight = input.nextDouble();
+		System.out.print("Enter height in inches: ");
+		double height = input.nextDouble();
+		final double KILOGRAMS_PER_POUND = 0.45359237;
+		final double METERS_PER_INCH = 0.0254;
+		double weightInKilograms = weight * KILOGRAMS_PER_POUND;
+		double heightInMeters = height * METERS_PER_INCH;
+		double BMI = weightInKilograms/(heightInMeters*heightInMeters);
+		System.out.println("BMI is "+BMI);
+		if(BMI<18.5)
+			System.out.println("Underweight!");
+		else if(BMI<25)
+			System.out.println("Normal!");
+		else if(BMI<30)
+			System.out.println("Overweight!");
+		else
+			System.out.println("Obese!");
+	}
+}
